acc3340af628474ee99c6d3d0551b70c4ac998a3
matheus-hdas <matheussantos.betim@gmail.com>
2024-10-29 17:39:03 -0300

    add `Mission` model class

---
+++ src/main/java/dev/matheus_hdas/ninjaregister/mission/model/Mission.java
@@ -0,0 +1,25 @@
+package dev.matheus_hdas.ninjaregister.mission.model;
+
+import dev.matheus_hdas.ninjaregister.ninja.model.Ninja;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_mission")
+public class Mission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String rank;
+
+    @OneToMany
+    private List<Ninja> ninjas;
+
+
+}
