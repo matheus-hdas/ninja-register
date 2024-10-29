6aa934d4d9f520039c2527602dc6a23713fed44a
matheus-hdas <matheussantos.betim@gmail.com>
2024-10-29 18:13:15 -0300

    feat: add `lombok` for abstraction

--- src/main/java/dev/matheus_hdas/ninjaregister/mission/model/Mission.java
+++ src/main/java/dev/matheus_hdas/ninjaregister/mission/model/Mission.java
@@ -2,11 +2,17 @@ package dev.matheus_hdas.ninjaregister.mission.model;
 
 import dev.matheus_hdas.ninjaregister.ninja.model.Ninja;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_mission")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Mission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +26,4 @@ public class Mission {
 
     @OneToMany(mappedBy = "mission")
     private List<Ninja> ninjas;
-
-
 }
--- src/main/java/dev/matheus_hdas/ninjaregister/ninja/model/Ninja.java
+++ src/main/java/dev/matheus_hdas/ninjaregister/ninja/model/Ninja.java
@@ -2,11 +2,17 @@ package dev.matheus_hdas.ninjaregister.ninja.model;
 
 import dev.matheus_hdas.ninjaregister.mission.model.Mission;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_ninja")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ninja {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,48 +30,4 @@ public class Ninja {
     @ManyToOne
     @JoinColumn(name = "id_mission")
     private Mission mission;
-
-    public Ninja(String name, String emal, int age) {
-        this.name = name;
-        this.email = emal;
-        this.age = age;
-    }
-    public Ninja() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ninja ninja = (Ninja) o;
-        return age == ninja.age && Objects.equals(name, ninja.name) && Objects.equals(email, ninja.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, email, age);
-    }
 }
